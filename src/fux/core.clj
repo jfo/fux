4b1cfc255aa52df12054d35dd441628c60eef2f8
urthbound <jeffowler@gmail.com>
2014-03-16 20:57:47 -0400

    preclean

--- src/fux/core.clj
+++ src/fux/core.clj
@@ -4,14 +4,14 @@
             [fux.parser :as p]
             ; [fux.scheduler :as sched]
             ; [fux.player :as play]
-            [fux.spinetokenparser :as s]
+            ; [fux.spinetokenparser :as s]
             [fux.tuners :as tuner]
             [clojure.java.io :as io]))
 
 (use 'clojure.pprint)
 
 (def kern
-  (with-open [rdr (io/reader "/Users/jeff/code/fux/resources/bach-chorales/249.krn")]
+  (with-open [rdr (io/reader "/Users/jeff/code/fux/resources/bach-chorales/200.krn")]
     (->> (line-seq rdr)
          (into [] ))))
 
@@ -23,31 +23,19 @@
 ; (p/split-spines kern)
 ; (p/tokenize-spine (p/split-spines kern))
 ; (pprint (p/parse-kern kern))
+; (pprint tuner/pythags)
+; (pprint (tuner/equaltemp))
 
-(pprint tuner/pythags)
-(tuner/equaltemp)
 
-
-(ot/definst sin-wave [freq 440 attack 0.1 sustain 0.1 release 0.5 vol 0.5] 
+(ot/definst sin-wave [freq 440 attack 0.5 sustain 0.2 release 1.1 vol 0.1]
   (* (ot/env-gen (ot/lin attack sustain release) 1 1 0 1 ot/FREE)
      (ot/sin-osc freq)
      vol))
 
-(ot/definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4] 
+(ot/definst square-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
   (* (ot/env-gen (ot/lin attack sustain release) 1 1 0 1 ot/FREE)
      (ot/lf-pulse:ar freq)
      vol))
-
-(sin-wave)
-(ot/stop)
-
-; plays note immediately
-; (ot/definst midi [note 60 amp 0.3]
-;    (let [freq (ot/midicps note)]
-;       (* amp
-;           (ot/env-gen (ot/perc 0.2 0.2) 1 1 0 1 :action ot/FREE)
-;           (+ (ot/sin-osc (/ freq 1))))))
-
 
 ; now i need a function that, given a metronome marking and an amount of note offset (quarter note = 1) defines an absolute time
 (defn schedule-note
@@ -58,27 +46,23 @@
 
 (defn playsched [note offset start]
      (ot/at (+ start offset)
-            (square-wave (tuner/pythags note))))
-
-; (playsched 60 1000)
+            (sin-wave ((tuner/equaltemp) note))))
 
 ; now I just need to apply that to all the notes in a spine.
 (defn schedule-spine [spine now]
   (map #(playsched (% :notecode) (schedule-note (% :offset)) now) spine))
-
 
 ; (def test-spine (first ((p/parse-kern kern) :spines)))
 ; (schedule-spine test-spine)
 ; (def test-note (nth (first ((p/parse-kern kern) :spines)) 5))
 ; (schedule-note 5.0)
 
-
 ;this one!!!
 (defn play-kern [kern]
   (map #(schedule-spine % (ot/now)) (kern :spines)))
-;this one!!!
 
 (play-kern (p/parse-kern kern))
+;this one!!!
 
 
 
